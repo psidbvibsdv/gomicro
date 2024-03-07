1a8d8fc7e30c11ab87d92b2627772aef5b4fd5b3
psidbvibsdv <stiltskin44@gmail.com>
2024-03-08 00:11:21 +0200

    updated broker service to interact with rabbitmq

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 const webPort = "80"
 
-type Config struct{}
+type Config struct {
+	Rabbit *amqp.Connection
+}
 
 func main() {
+	//try to connect to rabbitmq
 
-	app := Config{}
+	conn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	app := Config{
+		Rabbit: conn,
+	}
 
 	fmt.Printf("Staring broker service on port %s \n", webPort)
 
@@ -21,8 +36,36 @@ func main() {
 		Handler: app.routes(),
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backoff = time.Second * 1
+	var conn *amqp.Connection
+
+	//wait until rabbit is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			log.Println("rabbitmq is not ready yet")
+			counts++
+		} else {
+			log.Println("connected to rabbitmq")
+			conn = c
+			break
+		}
+		log.Println("backing off for ", backoff, " seconds")
+		time.Sleep(backoff)
+		backoff *= 2
+
+		if counts > 5 {
+			log.Println(err)
+			return nil, err
+		}
+	}
+	return conn, nil
 }
---
+++ broker-service/event/consumer.go
@@ -0,0 +1,135 @@
+package event
+
+import (
+	"bytes"
+	"encoding/json"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"net/http"
+)
+
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+func NewConsumer(conn *amqp.Connection) (*Consumer, error) {
+	consumer := Consumer{
+		conn: conn,
+	}
+
+	err := consumer.setup()
+	if err != nil {
+		return &Consumer{}, err
+	}
+
+	return &consumer, nil
+}
+
+func (c *Consumer) setup() error {
+	ch, err := c.conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	return declareExchange(ch)
+}
+
+type Payload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (c *Consumer) Listen(topics []string) error {
+	ch, err := c.conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	q, err := declareQueue(ch)
+	if err != nil {
+		return err
+	}
+
+	for _, t := range topics {
+		err = ch.QueueBind(
+			q.Name,       // queue name
+			t,            // routing key
+			"logs_topic", // exchange
+			false,        // no-wait
+			nil,          // arguments
+		)
+		if err != nil {
+			return err
+		}
+	}
+	messages, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // arguments
+	)
+
+	forever := make(chan bool)
+	go func() {
+		for d := range messages {
+			var payload Payload
+			_ = json.Unmarshal(d.Body, &payload)
+			log.Printf("Received a message: %s", d.Body)
+
+			go handlePayload(payload)
+		}
+	}()
+
+	log.Printf(" Waiting for messages. [Exchange, Queue] [logs_topic, %s]\n", q.Name)
+	<-forever
+	return nil
+}
+
+func handlePayload(payload Payload) {
+	switch payload.Name {
+	case "log", "event":
+		err := logEvent(payload)
+		if err != nil {
+			log.Println(err)
+		}
+	case "auth":
+	//authenticate
+
+	default:
+		err := logEvent(payload)
+		if err != nil {
+			log.Println(err)
+		}
+
+	}
+}
+
+func logEvent(entry Payload) error {
+	jsonData, _ := json.MarshalIndent(entry, "", " ")
+
+	logServiceURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusAccepted {
+		return err
+	}
+
+	return nil
+}
